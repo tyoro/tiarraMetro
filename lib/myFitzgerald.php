ff8cf11221464b7b72d13bae1ce0dce13b8b94c3
tyoro <orz@tyo.ro>
2011-12-27 02:04:16 +0900

    ファイルとコードを整理

--- lib/myFitzgerald.php
+++ lib/myFitzgerald.php
@@ -164,31 +164,3 @@
 		}
 	}
 
-	class ArrayUtil {
-		static public function isHash( $target ){
-			if (!is_array($target)) return false;
-
-			$result = true;
-
-			foreach ($target as $key => $value){
-				if(is_numeric($key)) {
-					$result = false;
-					break;
-				}
-			}
-
-			return $result;
-		}
-
-		static public function getWithKey($from, $key = 0){
-			$result = array();
-
-			foreach ((array)$from as $value){
-				if (is_array($value) && !empty($value[$key])) {
-					$result[] = $value[$key];
-				}
-			}
-
-			return $result;
-		}
-	}
