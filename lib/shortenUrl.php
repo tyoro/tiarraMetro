39c3c90b195a1f1f23d99a4f16060327bb4aeb35
arawas <arawas.jp@gmail.com>
2012-07-25 00:25:23 +0900

    add exeption procedure

--- lib/shortenUrl.php
+++ lib/shortenUrl.php
@@ -37,66 +37,74 @@
 	
 		public function shorten ($url='') {
 			if ($this->is_enabled) {
-				$this->uri = parse_url($url);
-				if ($this->uri['scheme'] == 'http' || $this->uri['scheme'] == 'https') {
-					if (!in_array($this->uri['host'], $this->shorten)) {
-						$this->uri = 'https://www.googleapis.com/urlshortener/v1/url?key=' . $this->api_key;
-	
-						$this->data = json_encode(array('longUrl' => $url));
-						$this->request = new HTTP_Request2(null);
-						$this->request->setUrl($this->uri);
-						$this->request->setConfig('ssl_verify_peer', false);
-						$this->request->setMethod(HTTP_Request2::METHOD_POST);
-						$this->request->setHeader('Content-Type', 'application/json');
-						$this->request->setBody($this->data);
-						$this->response = $this->request->send();
+				try {
+					$this->uri = parse_url($url);
+					if ($this->uri['scheme'] == 'http' || $this->uri['scheme'] == 'https') {
+						if (!in_array($this->uri['host'], $this->shorten)) {
+							$this->uri = 'https://www.googleapis.com/urlshortener/v1/url?key=' . $this->api_key;
 
-						if ($this->response->getStatus() == HTTP_STATUS_OK) {	
-							$this->json = json_decode($this->response->getBody());
+							$this->data = json_encode(array('longUrl' => $url));
+							$this->request = new HTTP_Request2(null);
+							$this->request->setUrl($this->uri);
+							$this->request->setConfig('ssl_verify_peer', false);
+							$this->request->setMethod(HTTP_Request2::METHOD_POST);
+							$this->request->setHeader('Content-Type', 'application/json');
+							$this->request->setBody($this->data);
+							$this->response = $this->request->send();
+
+							if ($this->response->getStatus() == HTTP_STATUS_OK) {	
+								$this->json = json_decode($this->response->getBody());
+							}
 						}
-					}
 
-					# if (trim($this->json->id) != '') {
-						return empty($this->json->id)?$url:$this->json->id;
-					# }
+						# if (trim($this->json->id) != '') {
+							return empty($this->json->id)?$url:$this->json->id;
+						# }
+					}
+				} catch (Exception $e) {
+					// 
 				}
 			}
 			return $url;
 		}
 		public function expand ($url='') {
 			if ($this->is_enabled) {
-				$this->uri = parse_url($url);
-				if ($this->uri['scheme'] == 'http' || $this->uri['scheme'] == 'https') {
-					if (in_array($this->uri['host'], $this->shorten)) {
-						if ($this->uri['host'] == 'goo.gl') {
-							$this->uri = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl=' . $url . '&key=' . $this->api_key;
-							$this->request = new HTTP_Request2(null);
-							$this->request->setUrl($this->uri);
-							$this->request->setConfig('ssl_verify_peer', false);
-							$this->request->setMethod(HTTP_Request2::METHOD_GET);
-							$this->response = $this->request->send();
+				try {
+					$this->uri = parse_url($url);
+					if ($this->uri['scheme'] == 'http' || $this->uri['scheme'] == 'https') {
+						if (in_array($this->uri['host'], $this->shorten)) {
+							if ($this->uri['host'] == 'goo.gl') {
+								$this->uri = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl=' . $url . '&key=' . $this->api_key;
+								$this->request = new HTTP_Request2(null);
+								$this->request->setUrl($this->uri);
+								$this->request->setConfig('ssl_verify_peer', false);
+								$this->request->setMethod(HTTP_Request2::METHOD_GET);
+								$this->response = $this->request->send();
 
-							if ($this->response->getStatus() == HTTP_STATUS_OK) {
-								$this->json = json_decode($this->response->getBody());
-							}
+								if ($this->response->getStatus() == HTTP_STATUS_OK) {
+									$this->json = json_decode($this->response->getBody());
+								}
 	
-							# if (trim($this->json->longUrl) != '') {
-								return empty($this->json->longUrl)?$url:$this->json->longUrl;
-							# }
-						} else {
-							$this->request = new HTTP_Request2(null);
-							$this->request->setUrl($url);
-							$this->request->setConfig('ssl_verify_peer', false);
-							$this->request->setMethod(HTTP_Request2::METHOD_HEAD);
-							$this->response = $this->request->send();
+								# if (trim($this->json->longUrl) != '') {
+									return empty($this->json->longUrl)?$url:$this->json->longUrl;
+								# }
+							} else {
+								$this->request = new HTTP_Request2(null);
+								$this->request->setUrl($url);
+								$this->request->setConfig('ssl_verify_peer', false);
+								$this->request->setMethod(HTTP_Request2::METHOD_HEAD);
+								$this->response = $this->request->send();
 
-							if ($this->response->getStatus() == 301 || $this->response->getStatus() == 302) {
-								$this->header = $this->response->getHeader();
+								if ($this->response->getStatus() == 301 || $this->response->getStatus() == 302) {
+									$this->header = $this->response->getHeader();
+								}
+
+								return empty($this->header['location'])?$url:$this->header['location'];
 							}
-
-							return empty($this->header['location'])?$url:$this->header['location'];
 						}
 					}
+				} catch (Exception $e) {
+					//
 				}
 			}
 			return $url;
